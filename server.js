11cdf27f75e4e3fb6ce4b719dd9fe66250856a9c
JohnzzzAntony <johnsjoantony@gmail.com>
2026-03-11 10:39:54 +0400

    Configure backend for hosting and dynamic API URL

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.post('/api/assets/:category', async (req, res) => {
     }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Node Server running on http://localhost:${PORT}`);
+    console.log(`Node Server running on port ${PORT}`);
 });
